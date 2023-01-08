f0e0c6aa63a9cfe8760092889c9640c628fcec90
Albina <latipova.alb@gmail.com>
2023-01-08 20:50:30 +0500

    prim

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 30000;
+        int milesPrice = 20;
+        int miles = ticketPrice / milesPrice;
+        System.out.println(miles + "начислено количество милей за купленный билет ");
+
+    }
+}
